2045967946b8f97a68caeb38d3cc00f5fe86c8c6
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2026-02-13 18:38:24 +0800

    Add button listener for nav buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import "./styles.css";
 import { generateHomeContent } from "./homepage.js";
 
+const contentDiv = document.getElementById("content");
+
+function clearContent() {
+    let child = contentDiv.lastElementChild;
+    while (child) {
+        contentDiv.removeChild(child);
+        child = contentDiv.lastElementChild;
+    }
+}
+
+const buttons = document.querySelector("button");
+buttons.addEventListener('click', (event) => {
+    let target = event.target;
+
+    switch (target.id) {
+        case "home":
+            clearContent();
+            generateHomeContent();
+            break;
+        case "menu":
+            break;
+        case "about":
+            break;
+    }
+});
+
 generateHomeContent();
